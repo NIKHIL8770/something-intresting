5a2a27a9b39a55f0d16cc939275d93b6c7998491
NIKHIL <nikhilprajapati870@gmail.com>
2024-09-18 20:17:31 +0530

    Add files via upload

---
+++ printvalueusingvariavle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int x = 90;
+  int* z = &x;
+  printf("%d\n",x);
+  printf("%p\n",&x);
+  printf("%p\n",z);
+  printf("%p\n",&z);
+  printf("%d\n",*z);// derefrence
+
+
+  return 0;
+}
